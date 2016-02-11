0d8bd2c6c09a24dcc3f4666f894ff0a30c3b4ff3
Andy Primawan <andyprimawan@gmail.com>
2016-02-11 12:56:45 +0700

    Update implementation for Drupal theme

--- js/zoneoke.js
+++ js/zoneoke.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-  var menuToggle = $('#js-mobile-menu').unbind();
-  $('#js-navigation-menu').removeClass("show");
+jQuery(document).ready(function() {
+  var menuToggle = jQuery('#js-mobile-menu').unbind();
+  jQuery('#js-navigation-menu').removeClass("show");
 
   menuToggle.on('click', function(e) {
     e.preventDefault();
-    $('#js-navigation-menu').slideToggle(function(){
-      if($('#js-navigation-menu').is(':hidden')) {
-        $('#js-navigation-menu').removeAttr('style');
+    jQuery('#js-navigation-menu').slideToggle(function(){
+      if(jQuery('#js-navigation-menu').is(':hidden')) {
+        jQuery('#js-navigation-menu').removeAttr('style');
       }
     });
   });
